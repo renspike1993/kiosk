c236b898f4fe2d686c62eed71deaa536a031c534
renspike1993 <cold.renspike@gmail.com>
2025-09-06 06:08:16 +0800

    passing the client inputs to server

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('admin/home.html')
 
+
+@app.route("/kiosk")
+def kiosk():
+    return render_template('client.html')
+
 if __name__ == "__main__":
     # Debug=True auto-reloads when you change code
     app.run(host="0.0.0.0", port=5000, debug=True)
